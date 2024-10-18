66de1d03f4ad35c6efe0ee0141aeade6b4d20bd0
manoelablanke <140423009+manoelablanke4@users.noreply.github.com>
2024-10-18 10:23:30 +0100

    Update application_layer.c

--- LAB1/code/src/application_layer.c
+++ LAB1/code/src/application_layer.c
@@ -2,8 +2,19 @@
 
 #include "application_layer.h"
 #include "link_layer.h"
+#include "string.h"
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
+    LinkLayer port;
+    LinkLayerRole port_role;
+    if(role == "tx") port_role= LlTx; else port_role=LlRx;
+    
+    strncpy(port.serialPort, serialPort, sizeof(port.serialPort) - 1);
+    port.baudRate = baudRate;
+    port.nRetransmissions = nTries;
+    port.timeout = timeout;
+
+    llopen(port);
 }

36aa777a65fced83fbdfcae804de19e88c50a6e5
“manoela.americo” <manoela.americo@inesctec.com>
2024-10-26 15:13:41 +0100

    llwrite updeated

--- LAB1/code/src/application_layer.c
+++ LAB1/code/src/application_layer.c
@@ -6,6 +6,11 @@
 #include <stdio.h>
 #include <string.h>
 
+#define NS_0 0x00
+#define NS_1 0x80
+
+unsigned char control = NS_0;
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
@@ -27,5 +32,6 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
         printf("Error opening connection\n");
         return; 
      }
+
 
 }
--- LAB1/code/src/link_layer.c
+++ LAB1/code/src/link_layer.c
@@ -16,9 +16,14 @@
 int alarmEnabled = FALSE;
 int alarmCount = 0;
 int send_times = 0;
+unsigned char ACTUAL;
 volatile int STOP = FALSE;
 LinkLayer ll;
 
+#define NS_0 0x00
+#define NS_1 0x80
+
+extern unsigned char control = 0x00;
 #define START 0 
 #define FLAG_RCV 1 
 #define A_RCV 2 
@@ -40,6 +45,12 @@ int llopen(LinkLayer connectionParameters)
     int fd =openSerialPort(connectionParameters.serialPort,
                        connectionParameters.baudRate);
     if (fd < 0) return 1;
+    ll.nRetransmissions = connectionParameters.nRetransmissions;
+    ll.timeout = connectionParameters.timeout;
+    ll.role = connectionParameters.role;
+    ll.baudRate = connectionParameters.baudRate;
+
+
     unsigned char buf[BUF_SIZE] = {0};
     unsigned char buf2[BUF_SIZE] = {0};
     unsigned char A, C, F;
@@ -189,35 +200,105 @@ int llopen(LinkLayer connectionParameters)
 // LLWRITE
 ////////////////////////////////////////////////
 int llwrite(const unsigned char *buf, int bufSize)
-{
+{ 
+    alarmCount = 0; send_times = 0;
     unsigned char stuffedFrame[256];
     size_t stuffedLength;
+    unsigned char wanted = 0xAB;
 
     byteStuffing(buf, bufSize, stuffedFrame, &stuffedLength);
-    stuffedFrame[0] = FLAG;
-    stuffedFrame[1] = 0x03; //always 0x03
-    stuffedFrame[2] = 0x00; //how am i suppose to know if its 0x00 or 0x80
-    stuffedFrame[3] = calculateBCC1(stuffedFrame[1], stuffedFrame[2]);
 
+    unsigned char transformedFrame[256];
+
+    transformedFrame[0] = FLAG;
+    transformedFrame[1] = 0x03;
+    transformedFrame[2] = control;
+    transformedFrame[3] = calculateBCC1(transformedFrame[1], transformedFrame[2]);
     
-    for(int i = 0; i < bufSize; i++){
-        stuffedFrame[i+4] = buf[i];
+    for(int i = 0; i < stuffedLength; i++){
+        transformedFrame[i+4] = stuffedFrame[i];
     }
 
-    stuffedFrame[bufSize+4] = calculateBCC2(buf, bufSize);
-    stuffedFrame[bufSize+5] = FLAG;
-    size_t newSize = bufSize + 6;
+    transformedFrame[stuffedLength+4] = FLAG;
+    transformedFrame[stuffedLength+5] = calculateBCC2(stuffedFrame, stuffedLength);
 
+    size_t transformedLength = stuffedLength + 6;
 
+    if(writeBytesSerialPort(transformedFrame, transformedLength) == transformedLength){send_times++;}
+    else { printf ("Error writing to serial port\n"); };
 
-    bool sent = false;
-    send_times = 0;
+    send_times++;
+    alarm(ll.timeout);
+    ACTUAL=START;
 
-    while(!sent && send_times < SEND_TIMES){
-        if(writeBytesSerialPort(stuffedFrame, stuffedLength) == stuffedLength){
-            sent = true;
+    unsigned char C;
+
+    while(STOP == FALSE && alarmCount < ll.nRetransmissions){   
+        readByteSerialPort(buf);
+        switch (ACTUAL){
+        case START:
+            printf("i recieved this %u and im at START\n", buf[0]);
+            if(buf[0] == FLAG) ACTUAL = FLAG_RCV;
+            break;
+        case FLAG_RCV:
+            printf("i recieved this %u im at FLAG_RCV\n", buf[0]);
+            if(buf[0] == FLAG) ACTUAL = FLAG_RCV;
+            else if(buf[0] == 0X03) ACTUAL = A_RCV;
+            else ACTUAL = START;
+            break;
+        case A_RCV:
+            printf("i recieved this %u and im at A_RCV\n", buf[0]);
+            if(buf[0] == 0XAA){ 
+                C = 0xAA;
+                ACTUAL = C_RCV;
+                control = 0x00;
+                }
+            else if(buf[0] == 0xAB){
+                C = 0xAB;
+                ACTUAL = C_RCV;
+                control = 0x80;
+            }
+            else if(buf[0] == 0x54){
+                printf("Received REJ\n");
+                send_times= 0;
+                writeBytesSerialPort(transformedFrame, transformedLength);
+                send_times++;
+                ACTUAL = START;
+            }
+            else if (buf[0]== 0x55){
+                printf("Received RJ\n");
+                send_times= 0;
+                writeBytesSerialPort(transformedFrame, transformedLength);
+                send_times++;
+                ACTUAL = START;
+            }
+            else ACTUAL = START;
+            break;
+        case C_RCV:
+        printf("i recieved this %u and im at C_RCV\n", buf[0]);
+            if(buf[0] == FLAG) ACTUAL = FLAG_RCV;
+            else if(buf[0] == calculateBCC1(0x03,C)) ACTUAL = BCC_OK;
+            else ACTUAL = START;
+            break;         
+
+        case BCC_OK:
+        printf("i recieved this %u and im at BCC_OK\n", buf[0]);
+            if(buf[0] == FLAG) STOP=TRUE;
+            else ACTUAL = START;
+            break;
+        default:
+            break;
         }
-        
+        if(STOP==TRUE) {
+            alarm(0);
+            printf("Received message is correct\n");
+        } else if(alarmCount > 0 && alarmCount == send_times){
+            printf("Resending message\n");
+            //PREPARE FRAME
+            writeBytesSerialPort(transformedFrame, transformedLength);
+            send_times++;
+            alarm(3);
+        }
     }
 
 
